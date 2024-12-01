b78b73d087423b30b00b19018322d369ff9c4543
wbasson <326238+willembasson@users.noreply.github.com>
2024-12-01 22:25:19 +0000

    cleanup the rust a bit

--- 2024/rust/01/run/src/main.rs
+++ 2024/rust/01/run/src/main.rs
@@ -1,20 +1,21 @@
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    let mut v1: Vec<u64> = Vec::new();
-    let mut v2: Vec<u64> = Vec::new();
-    let mut lines = INPUT.lines();
-    while let Some(line) = lines.next() {
+    let mut v1: Vec<usize> = Vec::new();
+    let mut v2: Vec<usize> = Vec::new();
+    let lines = INPUT.lines();
+    for line in lines {
         let mut it = line.split_whitespace();
-        let first = it.next().unwrap().parse::<u64>();
-        let last = it.next().unwrap().parse::<u64>();
-        v1.push(first.clone().unwrap());
-        v2.push(last.clone().unwrap());
+        let first = it.next().unwrap().parse::<usize>().unwrap();
+        let last = it.next().unwrap().parse::<usize>().unwrap();
+        v1.push(first);
+        v2.push(last);
     }
 
-    let mut sum: u64 = 0;
-    for i in 0..v1.len() {
-        sum += v1[i] * (v2.iter().filter(|&n| *n == v1[i]).count()) as u64;
+    let mut sum: usize = 0;
+    for i in v1 {
+        sum += i * (v2.iter().filter(|&n| *n == i).count());
     }
+
     println!("{}", sum);
 }
